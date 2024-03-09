f9ae525fa37d125bace29d4075ff8e985e57663d
anton.bashirov <anton.sh.local@gmail.com>
2024-03-09 21:10:02 +0200

    fixes

--- core/native/core/core.c
+++ core/native/core/core.c
@@ -1,7 +1,6 @@
 #include "core.h"
 #include <events/events.h>
 #include <modules/modules.h>
-#include <panic/panic.h>
 #include <system/network.h>
 #include <system/scheduling.h>
 #include <system/socket.h>
@@ -9,8 +8,6 @@
 #include <system/threading.h>
 #include <system/time.h>
 #include <system/types.h>
-#include "crash/crash.h"
-#include "printer/printer.h"
 
 void core_initialize(struct core_module_configuration* configuration)
 {
@@ -20,5 +17,4 @@ void core_initialize(struct core_module_configuration* configuration)
         system_default_event_raiser,
         system_default_event_printer,
         configuration->print_level);
-    print_event(core_event(event_new_information_empty("test")));
 }
