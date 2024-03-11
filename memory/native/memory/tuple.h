9744ca1a8273144f2b67e38ed4398ba77f2b447e
anton.bashirov <anton.sh.local@gmail.com>
2024-03-11 03:55:37 +0200

    fixes

--- memory/native/memory/tuple.h
+++ memory/native/memory/tuple.h
@@ -1,6 +1,8 @@
 #ifndef MEMORY_TUPLE_H
 #define MEMORY_TUPLE_H
 
+#include <common/common.h>
+#include <msgpuck.h>
 #include <system/library.h>
 
 #if defined(__cplusplus)
@@ -8,7 +10,12 @@ extern "C"
 {
 #endif
 
-uint64_t memory_tuple_next(const char* buffer, uint64_t offset);
+DART_INLINE uint64_t memory_tuple_next(const char* buffer, uint64_t offset)
+{
+    const char* offset_buffer = buffer + offset;
+    mp_next(&offset_buffer);
+    return (uint64_t)(offset_buffer - buffer);
+}
 
 #if defined(__cplusplus)
 }
