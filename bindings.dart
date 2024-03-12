c13cbcc1900c0200ccf55497590b4c290999ef9d
anton.bashirov <anton.sh.local@gmail.com>
2024-03-13 00:31:29 +0200

    fixe

--- bindings.dart
+++ bindings.dart
@@ -2,7 +2,7 @@ import 'dart:io';
 
 void main(List<String> args) {
   if (args.isNotEmpty) {
-    final process = Process.runSync("dart", ["run", "../infrastructure/dart/bindings.dart"], workingDirectory: Directory.current.path + '/${args.first}');
+    final process = Process.runSync("dart", ["run", "../infrastructure/dart/generators/bindings.dart"], workingDirectory: Directory.current.path + '/${args.first}');
     if ((process.stdout.toString()).isNotEmpty) print(process.stdout);
     if ((process.stderr.toString()).isNotEmpty) print(process.stderr);
     return;
@@ -10,7 +10,7 @@ void main(List<String> args) {
   Directory.current.listSync().forEach((element) {
     if (element.statSync().type == FileSystemEntityType.directory) {
       if ((element as Directory).listSync().any((element) => element.path.endsWith("native"))) {
-        final process = Process.runSync("dart", ["run", "../infrastructure/dart/bindings.dart"], workingDirectory: element.path);
+        final process = Process.runSync("dart", ["run", "../infrastructure/dart/generators/bindings.dart"], workingDirectory: element.path);
         if ((process.stdout.toString()).isNotEmpty) print(process.stdout);
         if ((process.stderr.toString()).isNotEmpty) print(process.stderr);
       }
