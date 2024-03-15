3a4379ef23d870c6349386b888238647e1771633
anton.bashirov <anton.sh.local@gmail.com>
2024-03-16 01:29:20 +0200

    fixes

--- memory/dart/lib/memory/tuples.dart
+++ memory/dart/lib/memory/tuples.dart
@@ -299,19 +299,3 @@ class MemoryTupleDynamicStreams {
     );
   }
 }
-
-void main(List<String> args) {
-  launch((creator) => creator.create(CoreModule(), CoreDefaults.module).create(MemoryModule(), MemoryDefaults.module)).activate(() {
-    final data = [123];
-    final sw = Stopwatch();
-    final stream = context().tuples().dynamic.output();
-    sw.start();
-    for (var i = 0; i < 1000; i++) {
-      stream.writeList(data.length);
-      stream.writeInt(data[0]);
-    }
-    stream.flush();
-    print(sw.elapsedMicroseconds);
-    stream.destroy();
-  });
-}
