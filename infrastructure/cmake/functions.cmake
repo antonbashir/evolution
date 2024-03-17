554b453192d4b468efaa4ce6089d469f63e65f67
anton.bashirov <anton.sh.local@gmail.com>
2024-03-17 12:20:59 +0200

    fixes

--- infrastructure/cmake/functions.cmake
+++ infrastructure/cmake/functions.cmake
@@ -2,6 +2,10 @@ function(copy_to_assets library)
   add_custom_command(TARGET ${library} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy $<TARGET_FILE:${library}> ${CMAKE_CURRENT_SOURCE_DIR}/../dart/assets/lib${library}.so)
 endfunction()
 
+function(copy_file_to_assets target from to)
+  add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${from} ${to})
+endfunction()
+
 function(list_add_prefix list_in prefix list_out)
   set(result "")
 
