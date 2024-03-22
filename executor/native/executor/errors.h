483cb24f37b4d29e6eb6eafec8ec8ad38162764d
anton.bashirov <anton.sh.local@gmail.com>
2024-03-22 21:56:10 +0200

    fixes

--- executor/native/executor/errors.h
+++ executor/native/executor/errors.h
@@ -7,6 +7,8 @@ extern "C"
 #endif
 
 #define executor_error_ring_full(...) executor_module_event(event_error(event_field_code(0), event_field_message("io_ring sqe is full"), ##__VA_ARGS__))
+#define executor_error_out_of_memory(...) executor_module_event(event_out_of_memory(__VA_ARGS__))
+#define executor_error_system(code, ...) executor_module_event(event_system_error(-result, ##__VA_ARGS__))
 
 #if defined(__cplusplus)
 }
