730a9e6bbe0276cde48d28f732ad65f4b9e20e68
anton.bashirov <anton.sh.local@gmail.com>
2024-03-24 18:30:24 +0200

    array

--- core/native/arrays/pointer.h
+++ core/native/arrays/pointer.h
@@ -16,14 +16,11 @@ DART_STRUCTURE struct pointer_array
 #define POINTER_ARRAY_DEFAULT_CAPACITY 16
 #define POINTER_ARRAY_DEFAULT_RESIZE_FACTOR 2
 
-#define pointer_array_add(array, value) array = pointer_array_add_resize(array, value)
-
 DART_INLINE_LEAF_FUNCTION struct pointer_array* pointer_array_create(size_t initial_capacity, size_t resize_factor)
 {
     struct pointer_array* array = (struct pointer_array*)calloc(1, sizeof(struct pointer_array));
     array->capacity = initial_capacity;
     array->memory = (void**)calloc(array->capacity, sizeof(void*));
-    array->capacity = initial_capacity;
     array->resize_factor = resize_factor;
     array->size = 0;
     return array;
@@ -40,15 +37,13 @@ DART_INLINE_LEAF_FUNCTION void pointer_array_destroy(struct pointer_array* array
     free(array);
 }
 
-DART_INLINE_LEAF_FUNCTION struct pointer_array* pointer_array_resize(struct pointer_array* array)
+DART_INLINE_LEAF_FUNCTION void pointer_array_grow(struct pointer_array* array)
 {
     size_t new_capacity = round_up_to_power_of_two(array->resize_factor * array->capacity);
-    struct pointer_array* resized = pointer_array_create(new_capacity, array->resize_factor);
-    memcpy(resized->memory, array->memory, array->capacity * sizeof(void*));
-    resized->size = array->size;
-    pointer_array_destroy(array);
-    return resized;
+    array->memory = (void**)realloc((void*)array->memory, new_capacity * sizeof(void*));
+    array->capacity = new_capacity;
 }
+
 
 DART_INLINE_LEAF_FUNCTION void* pointer_array_get(struct pointer_array* array, size_t index)
 {
@@ -64,12 +59,35 @@ DART_INLINE_LEAF_FUNCTION void* pointer_array_set(struct pointer_array* array, s
     return current;
 }
 
-DART_INLINE_LEAF_FUNCTION struct pointer_array* pointer_array_add_resize(struct pointer_array* array, void* value)
+DART_INLINE_LEAF_FUNCTION void pointer_array_remove_range(struct pointer_array* array, size_t from, size_t count)
 {
-    if (array->size >= array->capacity) array = pointer_array_resize(array);
+    if (from + count >= array->capacity) return;
+    memmove(array->memory + from * sizeof(void*), array->memory + (from + count) * sizeof(void*), (array->size - from - count) * sizeof(void*));
+    array->size -= count;
+}
+
+DART_INLINE_LEAF_FUNCTION void* pointer_array_remove(struct pointer_array* array, size_t index)
+{
+    if (index >= array->capacity) return NULL;
+    void* current = array->memory[index];
+    pointer_array_remove_range(array, index, 1);
+    return current;
+}
+
+DART_INLINE_LEAF_FUNCTION void* pointer_array_remove_last(struct pointer_array* array)
+{
+    if (array->size == 0) return NULL;
+    void* current = array->memory[array->size - 1];
+    array->memory[array->size - 1] = NULL;
+    array->size--;
+    return current;
+}
+
+DART_INLINE_LEAF_FUNCTION void pointer_array_add(struct pointer_array* array, void* value)
+{
+    if (array->size >= array->capacity) pointer_array_grow(array);
     array->memory[array->size] = value;
     array->size++;
-    return array;
 }
 
 #endif
